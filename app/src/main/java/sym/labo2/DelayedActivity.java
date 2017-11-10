f1f7c4a8d16d293ad9b80bcaa5b9c25721e06057
arthur <arthur.passuello@epfl.ch>
2017-11-10 15:34:37 +0100

    Added comment

--- app/src/main/java/sym/labo2/DelayedActivity.java
+++ app/src/main/java/sym/labo2/DelayedActivity.java
@@ -33,6 +33,8 @@ public class DelayedActivity extends AppCompatActivity {
         dsr = new DelayedSendRequest(this.getApplicationContext());
         dsr.setCommunicationEventListener( new CommunicationEventListener(){
             public boolean handleServerResponse(String response) {
+                // TO DO : Parse response
+                Log.i(TAG, response);
                 resultText.setText(response);
                 return true;
             }
@@ -47,3 +49,30 @@ public class DelayedActivity extends AppCompatActivity {
         }
     }
 }
+
+/*
+    Response from server
+ 11-10 15:29:05.
+ 213 5253-5253/sym.labo2
+ I/DelayedActivity: Hello World!
+ PHP_SELF: /rest/index.php
+ GATEWAY_INTERFACE: CGI/1.1
+ SERVER_ADDR: 193.134.218.24
+ SERVER_NAME: sym.iict.ch
+ SERVER_SOFTWARE: Apache/2.2.22 (Ubuntu)
+ SERVER_PROTOCOL: HTTP/1.1
+ REQUEST_METHOD: POST
+ REQUEST_TIME: 1510324173
+ QUERY_STRING:
+ DOCUMENT_ROOT: /var/www/sym/web
+ HTTP_ACCEPT_ENCODING: gzip
+ HTTP_CONNECTION: Keep-Alive
+ HTTP_HOST: sym.iict.ch
+ HTTP_USER_AGENT: Dalvik/2.1.0 (Linux; U; Android 8.0.0; Android SDK built for x86 Build/OSR1.170901.027)REMOTE_ADDR: 10.192.91.251
+ REMOTE_PORT: 46770
+ SCRIPT_FILENAME: /var/www/sym/web/rest/index.phpSERVER_ADMIN: fabien.dutoit@heig-vd.ch
+ SERVER_PORT: 80
+ SERVER_SIGNATURE: <address>Apache/2.2.22 (Ubuntu) Server at sym.iict.ch Port 80</address>
+ SCRIPT_NAME: /rest/index.php
+ REQUEST_URI: /rest/txt
+ */
