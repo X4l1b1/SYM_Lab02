4c8a3bd50e70bfe7a3265bb1066c257c2f1c798e
Marie <mariepascalemdjo2@gmail.com>
2017-11-12 22:43:13 +0100

    objectActivity and objectSendRequest OK

--- app/src/main/java/sym/labo2/ObjectSendRequest.java
+++ app/src/main/java/sym/labo2/ObjectSendRequest.java
@@ -4,33 +4,25 @@
  */
 package sym.labo2;
 
-        import android.content.Context;
-        import android.content.IntentFilter;
-        import android.net.ConnectivityManager;
-        import android.net.NetworkInfo;
-        import android.os.AsyncTask;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.JsonWriter;
-        import android.util.Log;
-        import android.util.Pair;
-        import android.widget.Toast;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
-        import org.json.JSONException;
-        import org.json.JSONObject;
-
-
-        import java.io.BufferedReader;
-        import java.io.BufferedWriter;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.io.OutputStream;
-        import java.io.OutputStreamWriter;
-        import java.net.HttpURLConnection;
-        import java.net.URL;
-        import java.util.ArrayList;
-
-        import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.HttpsURLConnection;
 
 public class ObjectSendRequest {
 
